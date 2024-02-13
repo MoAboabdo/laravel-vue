aae1b40295919da9cb8346ec2ff79f332f99ea63
Mohammed Abo abdo <40036423+MoAboabdo@users.noreply.github.com>
2024-02-14 01:56:36 +0200

    fix some errors

--- ecbackend/routes/api.php
+++ ecbackend/routes/api.php
@@ -18,14 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-    
-    Route::apiResource('products', ProductController::class);
-
-
-    
-
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-
 });
+
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::apiResource('products', ProductController::class);
